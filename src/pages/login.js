34630b206775453ff073e9d23f30b588ade052d1
Franz Cordes-Eklund <franzcordeseklund@gmail.com>
2021-04-24 16:29:26 -0500

    created basic input form for email and password

--- src/pages/login.js
+++ src/pages/login.js
@@ -21,7 +21,46 @@ export default function Login() {
 
     return (
         <div className="container flex mx-auto max-w-screen-md items-center h-screen">
-            <p>Hello world</p>
+            <div className="flex w-3/5">
+                <img 
+                    src="/images/iphone-with-profile.jpeg" 
+                    alt="iPhone with Finsta"
+                />
+            </div>
+            <div className="flex flex-col w-2/5">
+                <h1 className="flex justify-center w-full">
+                    <img 
+                        src="/images/logo.png" 
+                        alt="Finstagram logo" 
+                        className="mt-2 w-6/12 mb-4"
+                    />
+                </h1>
+                
+                {error && <p className="mb-4 text-xs text-red-primary">{error}</p>}
+
+                <form onSubmit={handleLogin} method="POST">
+                    <input
+                        aria-label="Enter your email address"
+                        type="text"
+                        placeholder="Email address"
+                        className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 
+                        border border-gray-primary rounded mb-2"
+                        onChange={({ target }) => setEmailsAddress(target.value)}
+                    />
+                    <input
+                        aria-label="Enter your password"
+                        type="password"
+                        placeholder="Password"
+                        className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 
+                        border border-gray-primary rounded mb-2"
+                        onChange={({ target }) => setPassword(target.value)}
+                    />
+                </form>
+            </div>
         </div>
     );
 }
+
+// text-red-primary -> hex values
+// text-gray-base -> hex values
+// border-gray-primary -> hex values
