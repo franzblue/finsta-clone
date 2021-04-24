3afb3a01b5a90753ccfe26cdabb439c8eeb74eb1
Franz Cordes-Eklund <franzcordeseklund@gmail.com>
2021-04-24 12:30:59 -0500

    firebase database created

---
+++ src/lib/firebase.js
@@ -0,0 +1,24 @@
+/* eslint-disable */
+
+import Firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+// here I want to import the seed file
+
+const config = {
+    apiKey: "AIzaSyAuHspol2HH89OZGCF0Hi-pOvIFGdANnTM",
+    authDomain: "finsta-12321.firebaseapp.com",
+    projectId: "finsta-12321",
+    storageBucket: "finsta-12321.appspot.com",
+    messagingSenderId: "517660890927",
+    appId: "1:517660890927:web:edd6c755a77f1514ba33da"
+};
+
+const firebase = Firebase.initializeApp(config);
+const { FieldValue } = Firebase.firestore;
+
+// here is where I want to call the seed file (only ONCE!)
+// seedDatabase(firebase);
+
+export { firebase, FieldValue };
