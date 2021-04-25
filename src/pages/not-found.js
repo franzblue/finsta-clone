cbad0c049a7647b8a34115dea4bc9d9c1e26cabc
Franz Cordes-Eklund <franzcordeseklund@gmail.com>
2021-04-24 23:37:07 -0500

    created NotFound route for unassignet paths

---
+++ src/pages/not-found.js
@@ -0,0 +1,16 @@
+/* eslint-disable */
+import { useEffect } from 'react';
+
+export default function NotFound() {
+    useEffect(() => {
+        document.title = 'Not Found - Finstagram'
+    }, []);
+
+    return (
+        <div className="bg-gray-background">
+            <div className="mx-auth max-w-screen-lg">
+                <p className="text-center text-2xl">Not Found!</p>
+            </div>
+        </div>
+    );
+}
