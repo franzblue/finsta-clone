9cc5ff540ecb313db7dd74d95aa523d401ad1aea
Franz Cordes-Eklund <franzcordeseklund@gmail.com>
2021-04-24 20:05:25 -0500

    made edit to logo image, create link to sign up

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,6 +1,6 @@
 /* eslint-disable */
 import { useState, useContext, useEffect } from 'react';
-import { useHistory } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import FirebaseContext from '../context/firebase';
 
 export default function Login() {
@@ -23,44 +23,68 @@ export default function Login() {
         <div className="container flex mx-auto max-w-screen-md items-center h-screen">
             <div className="flex w-3/5">
                 <img 
-                    src="/images/iphone-with-profile.jpeg" 
+                    src="/images/iphone-with-profile.jpg" 
                     alt="iPhone with Finsta"
                 />
             </div>
             <div className="flex flex-col w-2/5">
-                <h1 className="flex justify-center w-full">
-                    <img 
-                        src="/images/logo.png" 
-                        alt="Finstagram logo" 
-                        className="mt-2 w-6/12 mb-4"
-                    />
-                </h1>
-                
-                {error && <p className="mb-4 text-xs text-red-primary">{error}</p>}
+                <div className="flex flex-col items-center bg-white p-4 border border-gray-primary mb-4">
+                    <h1 className="flex justify-center w-full">
+                        <img 
+                            src="/images/logo.png" 
+                            alt="Finstagram logo" 
+                            className="mt-2 w-6/12 mb-4"
+                        />
+                    </h1>
+                    
+                    {error && <p className="mb-4 text-xs text-red-primary">{error}</p>}
 
-                <form onSubmit={handleLogin} method="POST">
-                    <input
-                        aria-label="Enter your email address"
-                        type="text"
-                        placeholder="Email address"
-                        className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 
-                        border border-gray-primary rounded mb-2"
-                        onChange={({ target }) => setEmailsAddress(target.value)}
-                    />
-                    <input
-                        aria-label="Enter your password"
-                        type="password"
-                        placeholder="Password"
-                        className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 
-                        border border-gray-primary rounded mb-2"
-                        onChange={({ target }) => setPassword(target.value)}
-                    />
-                </form>
+                    <form onSubmit={handleLogin} method="POST">
+                        <input
+                            aria-label="Enter your email address"
+                            type="text"
+                            placeholder="Email address"
+                            className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 
+                            border border-gray-primary rounded mb-2"
+                            onChange={({ target }) => setEmailsAddress(target.value)}
+                        />
+                        <input
+                            aria-label="Enter your password"
+                            type="password"
+                            placeholder="Password"
+                            className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 
+                            border border-gray-primary rounded mb-2"
+                            onChange={({ target }) => setPassword(target.value)}
+                        />
+                        <button
+                            disabled={isInvalid}
+                            type="submit"
+                            className={
+                                `bg-blue-500 text-white w-full rounded h-8 font-bold
+                                ${isInvalid && 'opacity-50'}`
+                            }
+                        >Log In
+                        </button>
+
+                    </form>
+                </div>
+                <div className="flex justify-center items-center flex-col 
+                w-full bg-white p-4 border border-gray-primary">
+                    <p className="text-sm">
+                        Don't have an account?{` `}
+                        <Link to="/signup" className="font-bold text-blue-medium">
+                            Sign up
+                        </Link>
+                    </p>
+                </div>
             </div>
         </div>
     );
 }
 
+// TODO: add to tailwind config
+// bg-blue-medium -> hex values
 // text-red-primary -> hex values
+// text-blue-medium -> hex values
 // text-gray-base -> hex values
 // border-gray-primary -> hex values
