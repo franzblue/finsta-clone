b0cbd15c3d2bf5925b3ff436ec14a361b58f10c8
Franz Cordes-Eklund <franzcordeseklund@gmail.com>
2021-04-26 14:35:36 -0500

    created a post component. currently has an index.js and a header.js displaying username and profile picture

---
+++ src/components/post/index.js
@@ -0,0 +1,26 @@
+import { useRef } from 'react';
+import PropTypes from 'prop-types';
+import Header from './header';
+
+export default function Post({ content }) {
+    // components
+    // -> header, image, actions (like & comment icons), footer, comments
+
+    return <div className="rounded col-span-4 border bg-white border-gray-primary mb-16">
+            <Header username={content.username} />
+        </div>
+}
+
+// data validation
+Post.propTypes = {
+    content: PropTypes.shape({
+        username: PropTypes.string.isRequired,
+        imageSrc: PropTypes.string.isRequired,
+        caption: PropTypes.string.isRequired,
+        docId: PropTypes.string.isRequired,
+        userLikedPhoto: PropTypes.bool.isRequired,
+        likes: PropTypes.array.isRequired,
+        comments: PropTypes.array.isRequired,
+        dateCreated: PropTypes.number.isRequired
+    })
+};
