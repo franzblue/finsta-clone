7cdb7c96b2903d13ca304357752502a187bc992d
Franz Cordes-Eklund <franzcordeseklund@gmail.com>
2021-04-26 11:38:14 -0500

    lots of work was done to get suggested users displaying on the sidebar. still not finished

--- src/components/sidebar/user.js
+++ src/components/sidebar/user.js
@@ -1,11 +1,10 @@
 
-import { memo } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import Skeleton from 'react-loading-skeleton';
 
-const User = ({username, fullName}) =>
-    !username || !fullName ? (
+export default function User({username, fullName}) {
+    return !username || !fullName ? (
         <Skeleton count={1} height={61} />
     ) : (
         <Link to={`/p/${username}`} className="grid grid-cols-4 gap-4 mb-6 items-center">
@@ -22,9 +21,7 @@ const User = ({username, fullName}) =>
             </div>
         </Link>
     );
-
-export default memo(User);
-
+}
 
 User.propTypes = {
     username: PropTypes.string.isRequired,
